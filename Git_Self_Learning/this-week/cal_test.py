d994c593c7823f8b4414e7c8288f153157bf0b27
DESKTOP-RJMLUJQ\user <lucky90322@gmail.com>
2018-10-26 11:35:52 +0800

    Update cal_test.py

--- Git_Self_Learning/this-week/cal_test.py
+++ Git_Self_Learning/this-week/cal_test.py
@@ -6,7 +6,7 @@ class TestCal(unittest.TestCase):
     '''Test cal.py'''
     def test_addstr(self):
         '''Test method addstr(str1, str2)'''
-        self.assertEqual('applepen', appleAndPen('apple', 'pen'))
+        self.assertEqual('bananapen', appleAndPen('apple', 'pen'))
 
 if __name__ == '__main__':
     unittest.main()
