4a1cdcbd84d89205772309b53d2d9ce0165c17d9
DESKTOP-RJMLUJQ\user <lucky90322@gmail.com>
2018-10-26 11:27:01 +0800

    Add cal_test.py

---
+++ Git_Self_Learning/this-week/cal_test.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+import unittest
+from cal import appleAndPen
+
+class TestCal(unittest.TestCase):
+    '''Test cal.py'''
+    def test_addstr(self):
+        '''Test method addstr(str1, str2)'''
+        self.assertEqual('applepen', appleAndPen('apple', 'pen'))
+
+if __name__ == '__main__':
+    unittest.main()
