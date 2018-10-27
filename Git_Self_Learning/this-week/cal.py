12d2aebc778a015eaf27d118622a1a19ec9670c6
DESKTOP-RJMLUJQ\user <lucky90322@gmail.com>
2018-10-27 10:03:16 +0800

    Update cal.py

--- Git_Self_Learning/this-week/cal.py
+++ Git_Self_Learning/this-week/cal.py
@@ -1,3 +1,10 @@
+'''
+>>> appleAndPen('Apple', 'Banana')
+'AppleBanana'
+'''
 def appleAndPen(str1, str2):
     return str1 + str2
 
+if __name__ == '__main__':
+    import doctest
+    print(doctest.testmod())
